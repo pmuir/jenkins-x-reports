a13017a12a28440a168d43fc106a7ac5e21b7d1c
Pete Muir <pmuir@bleepbleep.org.uk>
2018-09-17 03:56:04 -0700

    Add code for sending to elastic search

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bytes"
+	json2 "encoding/json"
+	"errors"
 	"fmt"
+	"github.com/clbanning/mxj"
+	"github.com/clbanning/mxj/x2j"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 const maxUploadSize = 2 * 1024 * 1024 // 2 MB
@@ -14,6 +21,7 @@ const uploadPath = "/reports"
 const downloadPort = 8080
 const uploadPort = 8081
 const bind = "0.0.0.0"
+const url = "http://jenkins-x-reports-elasticsearch-client:9200/tests/junit/"
 
 func main() {
 	go uploadServer()
@@ -81,8 +89,74 @@ func uploadFileHandler() http.HandlerFunc {
 			log.Println(err)
 			return
 		}
+		if r.Header.Get("Content-Type") == "text/vnd.junit-xml" {
+			sendToElasticSearch(r.Body, r.URL.Path)
+		}
 		w.Write([]byte("SUCCESS"))
+
 	})
+}
+
+func sendToElasticSearch(reader io.Reader, path string) error {
+	_, json, err := x2j.XmlReaderToJson(reader)
+	if err != nil {
+		return err
+	}
+	json, err = toJson(json)
+	fmt.Printf("Successfully annnotated JUnit result with build info\n")
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if (resp.StatusCode >= 200 && resp.StatusCode < 300 ) {
+		fmt.Printf("Sent %s to %s\n", path, url)
+	} else {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return errors.New(fmt.Sprintf("HTTP status: %s; HTTP Body: %s\n", resp.Status, body))
+	}
+	return nil
+}
+
+func toJson(json []byte) ([]byte, error) {
+	m, err := mxj.NewMapJson(json)
+	if err != nil {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	// Kibana is quite restrictive in the way it accepts JSON, so just rebuild the JSON entirely!
+
+	utc, _ := time.LoadLocation("UTC")
+	data := map[string]interface{} {
+		"org": os.Getenv("ORG"),
+		"appName": os.Getenv("APP_NAME"),
+		"version": os.Getenv("VERSION"),
+		"errors": m.ValueOrEmptyForPathString("testsuite.-errors"),
+		"failures": m.ValueOrEmptyForPathString("testsuite.-failures"),
+		"testsuiteName": m.ValueOrEmptyForPathString("testsuite.-name"),
+		"skippedTests": m.ValueOrEmptyForPathString("testsuite.-skipped"),
+		"tests": m.ValueOrEmptyForPathString("testsuite.-tests"),
+		"time": m.ValueOrEmptyForPathString("testsuite.-time"),
+		"timestamp": time.Now().In(utc).Format("2006-01-02T15:04:05Z"),
+		// TODO Add the TestCases
+	}
+	fmt.Printf("%s", data)
+	return json2.Marshal(data)
 }
 
 func renderError(w http.ResponseWriter, message string, statusCode int) {
